1ddfcb7bd1e794386612c89b2788153330c49730
hanbaoyi <604594231@qq.com>
2019-08-18 22:00:18 +0800

    显式this，隐式this

--- Function-explanation/this.js
+++ Function-explanation/this.js
@@ -6,3 +6,18 @@ f1(1,2)
 f2(1,2)
 f3(1,2)
 f4(1,2)
+
+
+const fn = function(arg1,arg2){console.log(this,arg1,arg2)}
+const obj = {method: fn}
+const array = [function(text){console.log(this,text)},1,2]
+
+//显式this
+fn.call({name: 1}, 1,2)
+fn.bind({name: 1}, 1,2)()
+obj.method.call(obj, 1,2)
+
+//隐式this
+fn(1,2)						//fn.call('undefined',1,2)  //window
+obj.method(1,2)		//obj.method.call(obj,1,2)	//{method: fn}
+array[0]('hi')							//array[0].call(array,'hi')	//[function(text){console.log(this,text)},1,2]
