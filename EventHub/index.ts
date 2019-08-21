82fafb7b52d8e459a280985b068e58ac8e8cd8dd
hanbaoyi <604594231@qq.com>
2019-08-21 22:58:30 +0800

    完善typescript

--- EventHub/index.ts
+++ EventHub/index.ts
@@ -1,25 +1,28 @@
 class EventHub {
-  eventList = {}
+  //类型是对象，对象key是string,value是array,array里面是个无返回值的function，function的参数是任意类型
+  private eventList: { [key: string]: Array<(data: unknown) => void> } = {}
   //{
   // '贵阳晚报': [fn1,fn2,fn3]
   //  '贵州都市晚报': [fn1,fn2,fn3]
   // }
-  on(eventName, fn) {
+  on(eventName: string, fn: (data: unknown) => void) { //unknown只要用过一次类型就不能再改了，而any可以随便改
     //把fn推进this.eventList[eventName]数组里
     this.eventList[eventName] = this.eventList[eventName] || []
     this.eventList[eventName].push(fn)
   }
-  emit(eventName, message?) {
+
+  emit(eventName: string, message?: unknown) {
     //把this.eventList[eventName]数组里面的fn全部依次调用
-    this.eventList[eventName] && this.eventList[eventName].map(fn => fn.call(null,message))
+    this.eventList[eventName] && this.eventList[eventName].map(fn => fn.call(null, message))
   }
-  off(eventName,fn){
+
+  off(eventName: string, fn: (data: unknown) => void) {
     //把fn从this.eventList[eventName]数组里删掉
     let index = this.eventList[eventName].indexOf(fn)
     console.log(this.eventList[eventName]); //[ [Function: fn2], [Function: fn3], [Function: fn1] ]
     console.log(index); //2
-    if(index === -1) return
-    this.eventList[eventName].splice(index,1)
+    if (index === -1) return
+    this.eventList[eventName].splice(index, 1)
     console.log(this.eventList[eventName]); //[ [Function: fn2], [Function: fn3] ]
   }
 }
--- EventHub/test.ts
+++ EventHub/test.ts
@@ -1,35 +1,42 @@
 import EventHub from "./index";
 
-const test1 = data => {
+type TestCase = (data: string) => void
+
+const test1: TestCase = data => {
   const eventHub = new EventHub()
   console.assert(eventHub instanceof Object, 'eventHub是个对象')
   console.log(data);
 }
 
 //on emit
-const test2 = data =>{
+const test2: TestCase = data => {
   const eventHub = new EventHub()
   let called = false
   eventHub.on('xxx', message => {
     called = true
-    console.assert(message === '今天林志玲结婚了')
+    console.assert(message[0] === '今天林志玲结婚了')
+    console.assert(message[1] === '言承旭无话可说')
+    console.log(message[0])
+    console.log(message[1])
   })
-  eventHub.emit('xxx', '今天林志玲结婚了')
+  eventHub.emit('xxx', ['今天林志玲结婚了', '言承旭无话可说'])
   setTimeout(() => {
     console.assert(called)
     console.log(data);
-  },1000)
+  }, 1000)
 }
 
 //off
-const test3 = data => {
+const test3: TestCase = data => {
   const eventHub = new EventHub()
   let called = false
   let fn1 = () => {
     called = true
   }
-  let fn2 = ()=> {}
-  let fn3 = ()=>{}
+  let fn2 = () => {
+  }
+  let fn3 = () => {
+  }
   eventHub.on('yyy', fn2)
   eventHub.on('yyy', fn3)
   eventHub.on('yyy', fn1)
@@ -38,10 +45,10 @@ const test3 = data => {
   setTimeout(() => {
     console.assert(!called)
     console.log(data);
-  },1000)
+  }, 1000)
 }
 
-test1('EventHub可以创建对')
+test1('EventHub可以创建对象')
 test2('on了之后，emit会触发on的函数')
 test3('off有用')
 
