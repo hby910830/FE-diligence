ecea54bec4eafaf54c78d5b65e11814a717289e5
hanbaoyi <604594231@qq.com>
2019-08-21 21:32:48 +0800

    基本完成EventHub

--- EventHub/index.ts
+++ EventHub/index.ts
@@ -1,5 +1,29 @@
-class EventHub{
+class EventHub {
+  eventList = {}
+  //{
+  // '贵阳晚报': [fn1,fn2,fn3]
+  //  '贵州都市晚报': [fn1,fn2,fn3]
+  // }
 
+  on(eventName, fn) {
+    //把fn推进this.eventList[eventName]数组里
+    if (!this.eventList[eventName]) {
+      this.eventList[eventName] = []
+    }
+    const array = this.eventList[eventName]
+    array.push(fn)
+  }
+
+  emit(eventName) {
+    //把this.eventList[eventName]数组里面的fn全部依次调用
+    let array = this.eventList[eventName]
+    if (!array) {
+      array = []
+    }
+    array.map(fn => {
+      fn.call()
+    })
+  }
 }
 
 export default EventHub
--- EventHub/test.ts
+++ EventHub/test.ts
@@ -3,3 +3,13 @@ import EventHub from "./index";
 const eventHub = new EventHub()
 
 console.assert(eventHub instanceof Object, 'eventHub是个对象')
+
+
+//on emit
+let called = false
+eventHub.on('xxx', () => {
+  called = true
+  console.log("called:" + called)
+})
+
+eventHub.emit('xxx')
