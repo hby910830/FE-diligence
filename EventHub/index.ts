85f2c3c048fe59dd71e5f13be0c2a7eaceca5bdc
hanbaoyi <604594231@qq.com>
2019-08-21 22:10:13 +0800

    完成eventHub.off功能

--- EventHub/index.ts
+++ EventHub/index.ts
@@ -9,9 +9,18 @@ class EventHub {
     this.eventList[eventName] = this.eventList[eventName] || []
     this.eventList[eventName].push(fn)
   }
-  emit(eventName, message) {
+  emit(eventName, message?) {
     //把this.eventList[eventName]数组里面的fn全部依次调用
     this.eventList[eventName] && this.eventList[eventName].map(fn => fn.call(null,message))
+  }
+  off(eventName,fn){
+    //把fn从this.eventList[eventName]数组里删掉
+    let index = this.eventList[eventName].indexOf(fn)
+    console.log(this.eventList[eventName]); //[ [Function: fn2], [Function: fn3], [Function: fn1] ]
+    console.log(index); //2
+    if(index === -1) return
+    this.eventList[eventName].splice(index,1)
+    console.log(this.eventList[eventName]); //[ [Function: fn2], [Function: fn3] ]
   }
 }
 
--- EventHub/test.ts
+++ EventHub/test.ts
@@ -13,5 +13,22 @@ eventHub.on('xxx', message => {
   console.log(message);
   console.assert(message === '今天林志玲结婚了')
 })
-
 eventHub.emit('xxx', '今天林志玲结婚了')
+
+
+const eventHub2 = new EventHub()
+let called2 = false
+let fn1 = () => {
+  called2 = true
+  console.log('called2: ' + called2);
+}
+let fn2 = ()=> {}
+let fn3 = ()=>{}
+eventHub2.on('yyy', fn2)
+eventHub2.on('yyy', fn3)
+eventHub2.on('yyy', fn1)
+eventHub2.off('yyy', fn1)
+eventHub2.emit('yyy')
+setTimeout(() => {
+  console.log('called2: ' + called2)
+},1000)
