e72c6a205f8a91187326bfec47cfa8ceea495db9
hanbaoyi <604594231@qq.com>
2019-08-21 22:25:37 +0800

    重构test测试用例

--- EventHub/test.ts
+++ EventHub/test.ts
@@ -1,34 +1,48 @@
 import EventHub from "./index";
 
-const eventHub = new EventHub()
-
-console.assert(eventHub instanceof Object, 'eventHub是个对象')
-
+const test1 = data => {
+  const eventHub = new EventHub()
+  console.assert(eventHub instanceof Object, 'eventHub是个对象')
+  console.log(data);
+}
 
 //on emit
-let called = false
-eventHub.on('xxx', message => {
-  called = true
-  console.log("called:" + called)
-  console.log(message);
-  console.assert(message === '今天林志玲结婚了')
-})
-eventHub.emit('xxx', '今天林志玲结婚了')
-
-
-const eventHub2 = new EventHub()
-let called2 = false
-let fn1 = () => {
-  called2 = true
-  console.log('called2: ' + called2);
+const test2 = data =>{
+  const eventHub = new EventHub()
+  let called = false
+  eventHub.on('xxx', message => {
+    called = true
+    console.assert(message === '今天林志玲结婚了')
+  })
+  eventHub.emit('xxx', '今天林志玲结婚了')
+  setTimeout(() => {
+    console.assert(called)
+    console.log(data);
+  },1000)
 }
-let fn2 = ()=> {}
-let fn3 = ()=>{}
-eventHub2.on('yyy', fn2)
-eventHub2.on('yyy', fn3)
-eventHub2.on('yyy', fn1)
-eventHub2.off('yyy', fn1)
-eventHub2.emit('yyy')
-setTimeout(() => {
-  console.log('called2: ' + called2)
-},1000)
+
+//off
+const test3 = data => {
+  const eventHub = new EventHub()
+  let called = false
+  let fn1 = () => {
+    called = true
+  }
+  let fn2 = ()=> {}
+  let fn3 = ()=>{}
+  eventHub.on('yyy', fn2)
+  eventHub.on('yyy', fn3)
+  eventHub.on('yyy', fn1)
+  eventHub.off('yyy', fn1)
+  eventHub.emit('yyy')
+  setTimeout(() => {
+    console.assert(!called)
+    console.log(data);
+  },1000)
+}
+
+test1('EventHub可以创建对')
+test2('on了之后，emit会触发on的函数')
+test3('off有用')
+
+
