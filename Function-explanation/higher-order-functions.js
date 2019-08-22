04902b638e3eb859b7ce9465d8628da02912bdb1
hanbaoyi <604594231@qq.com>
2019-08-22 23:23:24 +0800

    JS内置的高阶函数: Function.prototype.bind

---
+++ Function-explanation/higher-order-functions.js
@@ -0,0 +1,53 @@
+/*高阶函数:
+*把函数作为参数或者返回值的函数
+*/
+// JS内置的高阶函数：
+// 	1.Function.prototype.bind
+// 	2.Function.prototype.apply
+// 	3.Function.prototype.call
+// 	4.Function.prototype.sort
+// 	5.Function.prototype.map
+// 	6.Function.prototype.filter
+// 	7.Function.prototype.reduce
+
+const bind = Function.prototype.bind
+
+const f1 = function () {
+	console.log('this');
+	console.log(this)
+	console.log('arguments')
+	console.log(arguments)
+}
+const newF1 = f1.bind({name: 'hby'}, 1, 2, 3, 4)
+newF1()
+// this
+// {name: "hby"}
+// arguments
+// Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+/*推理*/
+// obj.method(a,b,c)
+// obj.method.call(obj,a,b,c)
+
+// 设 obj = f1
+// 设 method = bind
+
+// 代入
+// f1.bind(a,b,c)
+// f1.bind.call(f1,a,b,c)
+
+// 代入参数
+// f1.bind({name: 'hby'},1,2,3,)
+// f1.bind.call(f1,{name: 'hby'},1,2,3)
+
+// f1.bind = Function.prototype.bind
+// var bind  = Function.prototype.bind
+// 所以f1.bind = bind
+// bind.call(f1,{name: 'hby'},1,2,3)
+
+/*
+*bind.call接受一个函数f1,this,其他参数，返回一个新的函数，
+*新的函数会调用f1,并传入this和其他参数
+*/
+
+
