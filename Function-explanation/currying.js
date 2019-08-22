c7e95a4603e04bc481de7b25459ce9a25225a0d6
hanbaoyi <604594231@qq.com>
2019-08-22 22:18:29 +0800

    单参数函数接受两个参数

--- Function-explanation/currying.js
+++ Function-explanation/currying.js
@@ -1,15 +1,11 @@
-const addTwo = a => b =>  a + b
-const addThree = a => b => c =>  a + b + c
-const addFour = a => b => c => d =>  a + b + c + d
+//这是用对象的形式
+// const add = ({a,b}) => a +b
+// add({a:1,b:2}) //3
 
-const currify = fn => {
-	if(fn === addTwo){
-		return addTwo
-	}else if(fn === addThree) {
-		return addThree
-	}else if(fn === addFour){
-		return addFour
-	}
-}
 
-currify(addTwo)(1)(2)
+
+
+
+
+
+
