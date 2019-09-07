0cd823c691f8d802034ffbc7e1f72729d03b08a7
hanbaoyi <604594231@qq.com>
2019-09-07 23:25:09 +0800

    测试数组对象的深拷贝

--- DeepClone/index.js
+++ DeepClone/index.js
@@ -1,12 +1,20 @@
-function deepClone(source){
-	if(source instanceof Object){
-		const dist = {}
-		for (let key in source){
-			if(source.hasOwnProperty(key)){
+function deepClone(source) {
+	if (source instanceof Object) {
+		if (source instanceof Array) {
+			const dist = []
+			for (let key in source) {
 				dist[key] = deepClone(source[key])
 			}
+			return dist
+		} else {
+			const dist = {}
+			for (let key in source) {
+				if (source.hasOwnProperty(key)) {
+					dist[key] = deepClone(source[key])
+				}
+			}
+			return dist
 		}
-		return dist
 	}
 	return source
 }
--- DeepClone/test/test.js
+++ DeepClone/test/test.js
@@ -38,6 +38,15 @@ describe('deepClone是一个函数', () => {
 				assert(a.child !== b.child)
 				assert(a.child.name === b.child.name)
 			})
+			it('能够复制数组对象',() =>{
+				const a = [[1,2],[3,4],[5,6]]
+				const b = deepClone(a)
+				assert(a !== b)
+				assert(a[0] !== b[0])
+				assert(a[1] !== b[1])
+				assert(a[2] !== b[2])
+				assert.deepEqual(a,b)
+			})
 		})
 	})
 })
