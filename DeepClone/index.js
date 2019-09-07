49911b5291cd7b953bdfb57480d2b2dc982534b9
hanbaoyi <604594231@qq.com>
2019-09-08 00:14:09 +0800

    测试深拷贝

--- DeepClone/index.js
+++ DeepClone/index.js
@@ -1,5 +1,10 @@
+let cache = [] //缓存，为了解决环问题
 function deepClone(source) {
 	if (source instanceof Object) {
+		let cachedDist = findCache(source)
+		if(cachedDist){
+			return cachedDist
+		}
 		let dist
 		if (source instanceof Array) {
 			dist = []
@@ -10,6 +15,7 @@ function deepClone(source) {
 		} else {
 			dist = {}
 		}
+		cache.push([source,dist])
 		for (let key in source) {
 			if (source.hasOwnProperty(key)) {
 				dist[key] = deepClone(source[key])
@@ -18,6 +24,15 @@ function deepClone(source) {
 		return dist
 	}
 	return source
+}
+
+function findCache(source){
+	for(let i=0; i< cache.length;i++){
+		if(cache[i][0] === source){
+			return cache[i][1]
+		}
+	}
+	return undefined
 }
 
 module.exports = deepClone
