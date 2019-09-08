4818ee66c05ccee0544e44e390dbd126aba0cdde
hanbaoyi <604594231@qq.com>
2019-09-08 12:37:04 +0800

    安装@typs/ts-node @types/ts-node,完成测试用例代码构建

---
+++ Promise/test.ts
@@ -0,0 +1,17 @@
+import * as chai from 'chai'
+
+const assert = chai.assert
+
+describe('Chai的使用',() =>{
+  it('可以测试相等',() =>{
+    assert(1===1)
+  })
+})
+// describe('Promise',() => {
+//   it('是一个类',() => {
+//
+//   })
+//   it('拥有对象方法then',() => {
+//
+//   })
+// })
