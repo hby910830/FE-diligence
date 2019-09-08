20f74bdaf276d406745bd6ad9ee77c2d19e90e4e
hanbaoyi <604594231@qq.com>
2019-09-08 14:24:40 +0800

    使用assert.isFunction/assert.isObject

--- Promise/test.ts
+++ Promise/test.ts
@@ -5,8 +5,10 @@ import Promise from "./promise";
 
 describe('Promise',() => {
   it('是一个类',() => {
-    assert(typeof Promise === 'function')
-    assert(typeof Promise.prototype === 'object')
+    // assert(typeof Promise === 'function')
+    // assert(typeof Promise.prototype === 'object')
+    assert.isFunction(Promise)
+    assert.isObject(Promise.prototype)
   })
   // it('拥有对象方法then',() => {
   //
