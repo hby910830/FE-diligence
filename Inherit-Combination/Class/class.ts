37d6c22cade3c4e5044d2a6ba8b34e2d4395c2b7
hanbaoyi <604594231@qq.com>
2019-09-22 00:19:53 +0800

    public对公有属性进行简化

--- Inherit-Combination/Class/class.ts
+++ Inherit-Combination/Class/class.ts
@@ -1,19 +1,37 @@
 //对象的属性重复了，我们就可以用类和构造函数来解决重复问题
-class Person {
-  name: string
-  age: number
+// class Person {
+//   name: string
+//   age: number
+//
+//   sayHi(): void {
+//     console.log(this.name)
+//   }
+//
+//   constructor(name, age) {
+//     this.name = name
+//     this.age = age
+//   }
+// }
+//
+// const person1 = new Person('hby', 18)
+// person1.sayHi()
+// const person2 = new Person('韩宝亿', 28)
+// person2.sayHi()
 
+
+class Person {
   sayHi(): void {
     console.log(this.name)
   }
 
-  constructor(name, age) {
-    this.name = name
-    this.age = age
+  //public是公有属性，可以对this.name = name,this.age = age进行简化处理
+  constructor(public name: String, public age: Number) {
   }
 }
 
 const person1 = new Person('hby', 18)
-person1.sayHi()
+console.log(person1.age); //18
 const person2 = new Person('韩宝亿', 28)
-person2.sayHi()
+console.log(person2.age); //28
+console.log(person1.sayHi()); //hby
+console.log(person2.sayHi()); //韩宝亿
