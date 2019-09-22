83b01bec236be85425f7475177991ef8b0e714a3
hanbaoyi <604594231@qq.com>
2019-09-23 00:02:37 +0800

    组合模式---->狗型杀人机器人

--- Inherit-Combination/Combination/combined-mode.js
+++ Inherit-Combination/Combination/combined-mode.js
@@ -18,6 +18,12 @@ const createRun = state => ({
 	run: () => state.position += 1
 })
 
+const createKill = state => ({
+	kill: () => {
+		console.log(`哈哈，${state.name}被我杀死来！`)
+	}
+})
+
 const createDog = name => {
 	const state = {name, position: 0}  //闭包，外面拿不到state
 	return Object.assign( //浅复制地址，会把wangwang()和run()复制过来
@@ -33,3 +39,23 @@ console.log(dog.wangwang()) //汪汪，我是小白
 dog.run() //1
 dog.run() //2
 dog.run() //3
+
+
+
+/*狗型杀人机器人*/
+const createMurderRobotDog = name => {
+	const state = {name, position: 0}
+	return Object.assign(
+		{},
+		createWang(state),
+		createRun(state),
+		createKill(state),
+	)
+}
+
+const murderRobotDog = createMurderRobotDog('小白')
+console.log(murderRobotDog);						//{wangwang: ƒ, run: ƒ, kill: ƒ}
+console.log(murderRobotDog.wangwang());	//汪汪，我是小白
+console.log(murderRobotDog.run());			//1
+console.log(murderRobotDog.run());			//2
+console.log(murderRobotDog.kill());			//哈哈，小白被我杀死来！
