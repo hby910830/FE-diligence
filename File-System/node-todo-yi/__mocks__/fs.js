20392b90e81a55fd59a122c0b4bb628a8a1fe3b0
hanbaoyi <604594231@qq.com>
2019-10-16 23:17:27 +0800

    完成写文件测试

--- File-System/node-todo-yi/__mocks__/fs.js
+++ File-System/node-todo-yi/__mocks__/fs.js
@@ -1,18 +1,29 @@
 const fs = jest.genMockFromModule('fs') //jest提供的mock fs
 const _fs = jest.requireActual('fs')	//真实的fs
 Object.assign(fs, _fs)
-const mocks = []
-fs.setMock = (path, error, data) => {
-	mocks[path] = [error, data]
+const readMocks = []
+fs.setReadFileMock = (path, error, data) => {
+	readMocks[path] = [error, data]
 }
 //fs.readFile('xxx', fn)
 fs.readFile = (path, options, callback) => {
 	if (!callback) {callback = options} // 防止如果用户只传两个参数，就把callback赋值给第二个参数
-	if(path in mocks){
-		//callback(mocks[path][0],mocks[path][1]) //可以简写成下面的形式
-		callback(...mocks[path])
-	}else{
+	if (path in readMocks) {
+		//callback(readMocks[path][0],readMocks[path][1]) //可以简写成下面的形式
+		callback(...readMocks[path])
+	} else {
 		_fs.readFile(path, options, callback)
+	}
+}
+const writeMocks = {}
+fs.setWriteFileMock = (path, fn) => {
+	writeMocks[path] = fn
+}
+fs.writeFile = (path, data, options, callback) => {
+	if (path in writeMocks) {
+		writeMocks[path](path, data, options, callback)
+	}else{
+		_fs.writeFile(path, data, options, callback)
 	}
 }
 module.exports = fs
--- File-System/node-todo-yi/__test__/db.spec.js
+++ File-System/node-todo-yi/__test__/db.spec.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
 const db = require('../db')
 jest.mock('fs')
-
 describe('db', () => {
 	it('can read', async () => {
 		const data = [{title: 1, done: false}]
-		fs.setMock('./xxx', null, JSON.stringify(data))
+		fs.setReadFileMock('./xxx', null, JSON.stringify(data))
 		const list = await db.read('./xxx')
 		expect(list).toStrictEqual(data) //因为[]和[]是两个不同的对象，所以不能用toEqual()
+	})
+	it('can write', async () => {
+		let fakeFile
+		fs.setWriteFileMock('/yyy', (path, data, callback) => {
+			fakeFile = data
+			callback(null)
+		})
+		const list = [{title: '见欧阳娜娜', done: true},{title: '见迪丽热巴', done: true}]
+		await db.write(list,'/yyy')
+		expect(fakeFile).toStrictEqual(JSON.stringify(list) + '\n')
 	})
 })
