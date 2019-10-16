a44c5ff54bc05237db306ba53815d789b00950d3
hanbaoyi <604594231@qq.com>
2019-10-16 22:49:48 +0800

    完成读文件测试

--- File-System/node-todo-yi/__mocks__/fs.js
+++ File-System/node-todo-yi/__mocks__/fs.js
@@ -1,6 +1,18 @@
-const fs = jest.genMockFromModule('fs')
-fs.x = () => {
-	console.log('hi')
-	return 'xxx'
+const fs = jest.genMockFromModule('fs') //jest提供的mock fs
+const _fs = jest.requireActual('fs')	//真实的fs
+Object.assign(fs, _fs)
+const mocks = []
+fs.setMock = (path, error, data) => {
+	mocks[path] = [error, data]
+}
+//fs.readFile('xxx', fn)
+fs.readFile = (path, options, callback) => {
+	if (!callback) {callback = options} // 防止如果用户只传两个参数，就把callback赋值给第二个参数
+	if(path in mocks){
+		//callback(mocks[path][0],mocks[path][1]) //可以简写成下面的形式
+		callback(...mocks[path])
+	}else{
+		_fs.readFile(path, options, callback)
+	}
 }
 module.exports = fs
--- File-System/node-todo-yi/__test__/db.spec.js
+++ File-System/node-todo-yi/__test__/db.spec.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
+const db = require('../db')
 jest.mock('fs')
+
 describe('db', () => {
-	it('can read', () => {
-		expect(fs.x()).toBe('xxx')
+	it('can read', async () => {
+		const data = [{title: 1, done: false}]
+		fs.setMock('./xxx', null, JSON.stringify(data))
+		const list = await db.read('./xxx')
+		expect(list).toStrictEqual(data) //因为[]和[]是两个不同的对象，所以不能用toEqual()
 	})
 })
