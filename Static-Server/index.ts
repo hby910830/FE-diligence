7a80e4b760e7cbe0ce17b3664790a615c6cde53a
hanbaoyi <604594231@qq.com>
2019-10-17 23:15:58 +0800

    request: IncomingMessage, response: ServerResponse

--- Static-Server/index.ts
+++ Static-Server/index.ts
@@ -1,9 +1,16 @@
 import * as http from 'http'
+import {IncomingMessage, ServerResponse} from 'http';
+
 const server = http.createServer();
 
-server.on('request', (request,response) => {
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   console.log('有人请求了')
-  response.end('hi')
+  console.log(IncomingMessage)
+  console.log(ServerResponse);
+
+  response.end('hi' + '\n')
 })
 
-server.listen(8888)
+server.listen(8888, () => {
+  console.log(server.address())
+})
