abfc73546eed0daa7749d454a4bd41f8065b887d
hanbaoyi <604594231@qq.com>
2019-10-17 23:47:56 +0800

    response.statusCode/setHeader/write/end/, curl -v -d "xxx:xxx"

--- Static-Server/index.ts
+++ Static-Server/index.ts
@@ -11,13 +11,17 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   console.log(request.url);
   console.log(request.headers);
   let array = []
-  request.on('data', chunk =>{
+  request.on('data', chunk => {
     array.push(chunk)
   })
   request.on('end', () => {
     const body = Buffer.concat(array).toString()
     console.log(body);
-    response.end('hi' + '\n')
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'image/png')
+    response.setHeader('X-HBY', 'I am HanBaoYi')
+    response.write(1 + '\n')
+    response.end()
   })
 })
 
