09f4d21a448438181856ea30a9779f74d0d10f51
hanbaoyi <604594231@qq.com>
2019-10-24 23:40:21 +0800

    错误处理

--- Http-Request/src/main.ts
+++ Http-Request/src/main.ts
@@ -49,7 +49,22 @@ export const translate = wold => {
       }
       const object:BaiduResult = JSON.parse(string)
       if(object.error_code){
-        console.log(object.error_msg)
+        let table = {
+          52001: '请求超时',
+          52002: '系统错误',
+          52003: '未授权用户',
+          54000: '必填参数为空',
+          54001: '签名错误',
+          54003: '访问频率受限',
+          54004: '账户余额不足',
+          54005: '长query请求频繁',
+          58000: '客户端IP非法',
+          58001: '译文语言方向不支持',
+          58002: '服务当前已关闭',
+          90107: '认证未通过或未生效',
+        }
+        console.log(object.error_code);
+        console.log(table[object.error_code])
         process.exit(1)
       }else{
         console.log(object.trans_result[0].dst);
