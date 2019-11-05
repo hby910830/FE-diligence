045f937f7bdad787ce4c81ee70dc84636ad6bc77
hanbaoyi <604594231@qq.com>
2019-11-06 00:21:13 +0800

    init READM.md

--- Database-Operations/index.js
+++ Database-Operations/index.js
@@ -10,4 +10,4 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 	if (error) throw error;
 	console.log('The solution is: ', results[0].solution);
 });
-connection.end();
+connection.end();   
