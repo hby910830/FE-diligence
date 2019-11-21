8f49397a634d5ae9c5827fcd2d44cc0e5ef4a6ee
hanbaoyi <604594231@qq.com>
2019-11-22 01:08:17 +0800

    pipe管道

--- Stream/3.js
+++ Stream/3.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const server = http.createServer()
 server.on('request', (request, response) => {
 	const stream = fs.createReadStream('./bigFile.txt')
-	stream.pipe(response)
+	stream.pipe(response) //pipe 管道
 })
 server.listen(8888)
 console.log('localhost:8888')
@@ -11,4 +11,10 @@ console.log('localhost:8888')
 /*分析
 查看node.js内存占用，基本不会超过30Mb
 文件stream和response stream通过管道相连
+* */
+
+/*管道
+* 定义：两个流可以用一个管道相连，stream1的末尾连接上stream2的开端，只要stream1有数据，就会流到stream2
+* 常用代码：stream1.pipe(stream2)
+* 链式操作：a.pipe(b).pipe(c) === a.pipe(b);b.pipe(c)
 * */
