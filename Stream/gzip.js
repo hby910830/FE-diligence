cfcf2585e3b227331170c7d25096f92649c4b4c9
hanbaoyi <604594231@qq.com>
2019-11-29 00:21:14 +0800

    给一个....符号的进度条，监听finish之后打印Done

--- Stream/gzip.js
+++ Stream/gzip.js
@@ -5,4 +5,6 @@ const file = process.argv[2] //获取用户输入的文件路径参数
 
 fs.createReadStream(file)
 	.pipe(zlib.createGzip()) //进行gzip压缩操作
+	.on('data', () => process.stdout.write('.'))  //一个....符号的进度条
 	.pipe(fs.createWriteStream(file + '.gz')) //把读取的文件变成.gz文件
+	.on('finish', () => console.log('Done'))  //  结束之后打印 Done
