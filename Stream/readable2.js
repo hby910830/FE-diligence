24fdf85984d95176075ebd975696440d96c6097b
hanbaoyi <604594231@qq.com>
2019-11-28 23:54:39 +0800

    readable2.js添加注释：等别人调用read方法再推

--- Stream/readable2.js
+++ Stream/readable2.js
@@ -4,7 +4,7 @@ const inStream = new Readable({
 	read(size){
 		let char = String.fromCharCode(this.currentCharCode++)
 		this.push(char)
-		console.log(`退了${char}`)  //这是一个一个的推
+		console.log(`退了${char}`)  //这是一个一个的推,等别人调用read方法再推
 		if(this.currentCharCode > 90){ //Z
 			this.push(null)
 		}
