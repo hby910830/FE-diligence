553cd33ea01e42bcb641f2f8f35ede89f5d958ac
hanbaoyi <604594231@qq.com>
2019-11-29 00:28:44 +0800

    gzip.js配合transform stream

--- Stream/gzip.js
+++ Stream/gzip.js
@@ -3,8 +3,18 @@ const zlib = require('zlib')
 
 const file = process.argv[2] //获取用户输入的文件路径参数
 
+const {Transform} = require('stream')
+
+const reportProgress = new Transform({
+	transform(chunk, encoding, callback){
+		process.stdout.write('.')
+		callback(null,chunk)
+	}
+})
+
 fs.createReadStream(file)
 	.pipe(zlib.createGzip()) //进行gzip压缩操作
-	.on('data', () => process.stdout.write('.'))  //一个....符号的进度条
+	// .on('data', () => process.stdout.write('.'))  //一个....符号的进度条
+	.pipe(reportProgress)
 	.pipe(fs.createWriteStream(file + '.gz')) //把读取的文件变成.gz文件
 	.on('finish', () => console.log('Done'))  //  结束之后打印 Done
