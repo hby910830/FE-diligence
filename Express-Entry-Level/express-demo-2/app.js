52dce7071bbdddcf302c23c16f4b553ca8d24c54
hanbaoyi <604594231@qq.com>
2019-12-23 01:22:20 +0800

    路由

--- Express-Entry-Level/express-demo-2/app.js
+++ Express-Entry-Level/express-demo-2/app.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const logger = require('./logger')
 const app = express();
-
 app.use(logger('dev'))
 
-app.use(function (req, res, next) {
-	res.write('hi')
-	next()    //必须调用next才会往下执行
-});
-
-app.use(function (req, res, next) {
-	console.log(2)
-	res.write('hi')
+app.use((request, response, next) => {
+	if (request.path === '/' && request.method === 'GET') {
+		response.send('根目录')
+	}
 	next()
-});
+})
+
+//更简便的写法
+app.use('/xxx', (request, response, next) => {
+	response.send('xxx页面')
+	next()
+})
+
+app.get('/aaa', (request, response, next) => {
+	response.send('aaa页面')
+	next()
+})
+
+app.route('/bbb')
+	.all((request, response, next) => {})
+	.get((request, response, next) => {})
+	.post((request, response, next) => {})
+
+// app.use(function (req, res, next) {
+// 	res.write('hi')
+// 	next()    //必须调用next才会往下执行
+// });
+//
+// app.use(function (req, res, next) {
+// 	console.log(2)
+// 	res.write('hi')
+// 	next()
+// });
+//
 
 app.use(function (req, res, next) {
 	res.end()   //告诉浏览器响应结束了
